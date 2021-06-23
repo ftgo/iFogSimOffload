6859eaa1622d7f33f7d5ebeebd66c2398b207343
Felipe Toledo G Oliveira <felipetgoliveira@gmail.com>
2021-06-23 10:46:18 -0300

    Uploading simulation results

--- pidstat.sh
+++ pidstat.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+
+#pidstat -r -u -p $(pgrep -f ifogsim-jar-with-dependencies) 1>pidstat_`date +%Y%m%d%H%M%S`.log
 pidstat -r -u -p $(pgrep -f org.fog.examples.DataPlacement) 1>pidstat_`date +%Y%m%d%H%M%S`.log
